38e36f1c7cb67ca3534e1e8b836591dd58c89e5b
CuocThien <thiencuoc2210@gmail.com>
2022-03-06 22:19:26 +0700

    design component group

--- src/app/home/group/group.component.ts
+++ src/app/home/group/group.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-group',
   templateUrl: './group.component.html',
-  styleUrls: ['./group.component.scss']
+  styleUrls: ['../../../assets/sass/main.scss']
 })
 export class GroupComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  fileUpload(event: any) {
+    const selectedFile = event.target.files[0];
+    const fileReader = new FileReader();
+    fileReader.readAsBinaryString(selectedFile);
+    fileReader.onload = (event) => {
+      let binaryData = event.target?.result;
+      let workBook = XLSX.read(binaryData, { type: 'binary' })
+      let data = {}
+      workBook.SheetNames.forEach(sheet => {
+        Object.assign(data, XLSX.utils.sheet_to_json(workBook.Sheets[sheet]));
+      })
+    }
   }
 
 }
