5bf8912efde49ef86f46add63b8cde2e08835c39
CuocThien <thiencuoc2210@gmail.com>
2022-05-22 12:23:13 +0700

    fix: change host

--- src/app/utils/constant.ts
+++ src/app/utils/constant.ts
@@ -1,6 +1,6 @@
 const SERVER_HOST = 'https://schoolsocialnetwork.herokuapp.com'
 const LOCAL_HOST = 'http://localhost:3000'
-const HOST = LOCAL_HOST
+const HOST = SERVER_HOST
 
 const LIST_ROLE = [
     {
